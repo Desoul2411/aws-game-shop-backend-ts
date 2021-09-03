01c3066c07bda6b19222c89c32278c6412dfd492
Desoul2411 <Desoul2411@gmail.com>
2021-09-03 16:46:50 +0300

    add environment db variables serverless.ts

--- product-service-ts/serverless.ts
+++ product-service-ts/serverless.ts
@@ -23,10 +23,19 @@ const serverlessConfiguration: AWS = {
       shouldStartNameWithService: true,
     },
     environment: {
-      AWS_NODEJS_CONNECTION_REUSE_ENABLED: "1",
+      /* AWS_NODEJS_CONNECTION_REUSE_ENABLED: "1", */
+      PG_HOST: 'lesson4.ctpjnwanumqx.eu-west-1.rds.amazonaws.com',
+      PG_PORT: '5432',
+      PG_DATABASE: 'lesson4',
+      PG_USERNAME: 'postgress',
+      PG_PASSWORD: 'tp6UBi5yArHkNip8PNwX'
+
     },
     lambdaHashingVersion: "20201221",
   },
+
+    
+
   // import the function via paths
   functions: { getProductsList, getProductsById },
 };
