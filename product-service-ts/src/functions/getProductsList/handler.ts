9aebf174abd92b71094231761107698189ea1c28
Desoul2411 <Desoul2411@gmail.com>
2021-09-07 20:42:24 +0300

    rewrite getProductsList function/ add query to db

--- product-service-ts/src/functions/getProductsList/handler.ts
+++ product-service-ts/src/functions/getProductsList/handler.ts
@@ -2,10 +2,37 @@ import "source-map-support/register";
 import { formatJSONResponse } from "@libs/apiGateway";
 import { middyfy } from "@libs/lambda";
 import { APIGatewayProxyResult } from "aws-lambda";
-import goods from "../../data/goods.json";
+
+const { Client } = require("pg");
+
+const { PG_HOST, PG_PORT, PG_DATABASE, PG_USERNAME, PG_PASSWORD } = process.env;
+
+const dbOptions = {
+  user: PG_USERNAME,
+  host: PG_HOST,
+  database: PG_DATABASE,
+  password: PG_PASSWORD,
+  port: PG_PORT,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+  connectionTimeoutMillis: 5000,
+};
 
 const getProductsList = async (): Promise<APIGatewayProxyResult> => {
-  return formatJSONResponse(200, {products: goods});
+  const client = new Client(dbOptions);
+  await client.connect();
+
+  console.log('database',PG_DATABASE );
+
+  try {
+    const { rows } = await client.query("SELECT p.id, p.title, p.description, p.price, p.imageId, stocks.count FROM products as p LEFT OUTER JOIN stocks ON p.id = stocks.product_id");
+    return formatJSONResponse(200, { products: rows });
+  } catch (error) {
+    return formatJSONResponse(500, { error: error.stack });
+  } finally {
+    client.end();
+  }
 };
 
 export const main = middyfy(getProductsList);
